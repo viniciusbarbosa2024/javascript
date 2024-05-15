d7516392053a1c9c8ea29eefc5f39c86e716270e
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-15 20:43:21 -0300

    começando condicionais simples

---
+++ aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 90
+
+if (vel > 80) {
+    console.log(`Sua velociade é ${vel}km/h,acima do limite de 80km/h,por isso você irá receber uma multa`)
+}
+
+console.log('Use cinto de segurança')
