3e6458faa9dfdfedc0b540cf4d2a115271257cbf
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-17 20:48:36 -0300

    aula12

---
+++ aula12/ex013.js
@@ -0,0 +1,39 @@
+var agora = new Date
+var dia = agora.getDay() //Vai retornar o número equivalente ao dia da semana
+
+/* 
+    Domingo - 0
+    Segunda - 1
+    Terça - 2
+    Quarta - 3
+    Quinta - 4
+    Sexta - 5 
+    Sábado - 6
+*/
+
+switch (dia) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')        
+        break
+    case 2:
+        console.log('Terça')        
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Erro')
+        break
+}
