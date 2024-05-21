8e7cd18cddb532bf82b83a5c9b6b30c108ec26d6
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-20 22:00:35 -0300

    desenvolvendo projeto

---
+++ aula12ex/ex014/script.js
@@ -0,0 +1,18 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 14
+    msg.innerHTML = `São ${hora} horas`
+
+    if (hora>=0 && hora<12) {
+        img.src = 'imagens/manha.jpg'
+        document.body.style.backgroundColor = '#3BA3EC'
+    } else if (hora<18) {
+        img.src = 'imagens/tarde.jpg'
+        document.body.style.backgroundColor = '#935E63'
+    } else {
+        img.src = 'imagens/noite.jpg'
+    }
+}
