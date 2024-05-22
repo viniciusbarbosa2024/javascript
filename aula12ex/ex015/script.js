65aa1ec49c8fedac41031c029ec5a0f1077ecaff
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-22 20:31:05 -0300

    Terminei o exercício15

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -10,12 +10,32 @@ function Verificar() {
         var idade = anoat - ano.value
         var fsex = document.getElementsByName('sex')
         var genero = ''
+        var img = document.createElement('img')
         if (fsex[0].checked) {
             genero = 'homem'
+            if (idade >=0 && idade <10) {
+                img.setAttribute('src','imagens/menino.jpg')        
+            } else if (idade<21) {
+                img.setAttribute('src','imagens/homem-jovem.jpg')    
+            } else if (idade<50) {
+                img.setAttribute('src','imagens/homem-adulto.jpg')
+            } else {
+                img.setAttribute('src','imagens/homem-idoso.jpg')
+            }
         } else {
             genero = 'mulher'
+            if (idade >=0 && idade <10) {
+                img.setAttribute('src','imagens/menina.jpg')        
+            } else if (idade<21) {
+                img.setAttribute('src','imagens/mulher-jovem.jpg')    
+            } else if (idade<50) {
+                img.setAttribute('src','imagens/mulher-adulta.jpg')
+            } else {
+                img.setAttribute('src','imagens/mulher-idosa.jpg')
+            }
         }
         res.innerHTML = `Detectamos um ${genero} de ${idade} anos`
+        res.appendChild(img)
         res.style.textAlign = 'center'
         
     }
