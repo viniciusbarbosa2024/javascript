2cdef2837f01dfd37fe4430a92323d856fe7cc13
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-27 13:17:18 -0300

    Por enquanto é isso

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,8 +4,51 @@ function contagem() {
     var passo = document.getElementById('passo')
     var res = document.getElementById('res')
 
-    for (var c = Number(ini.value);c<=Number(fim.value);c+=Number(passo.value)) {
-        res.innerHTML += c
+    var i = Number(document.getElementById('ini').value)
+    var f = Number(document.getElementById('fim').value)
+    var p = Number(document.getElementById('passo').value)
+    
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = "Impossível contar!"
+    } else {
+        if(p != 0) {
+            if (i<f) {
+                res.innerHTML = ''
+                for (var c = i;c<=f;c+= p) {
+                    res.innerHTML += "&#x1F449 " + c
+                }
+                res.innerHTML += '&#x1F3C1 '
+            } else {
+                res.innerHTML = ''
+                for (var c=i;c>=f;c-= p) {
+                    res.innerHTML += "&#x1F449 " + c
+                }
+                res.innerHTML += '&#x1F3C1 '    
+            }  
+        } else {
+            window.alert('Passo inválido,considerando passo = 1')
+            p = 1
+            if (i<f) {
+                res.innerHTML = ''
+                for (var c = i;c<=f;c+= p) {
+                    res.innerHTML += "&#x1F449 " + c
+                }
+                res.innerHTML += '&#x1F3C1'
+            } else {
+                res.innerHTML = ''
+                for (var c=i;c>=f;c-= p) {
+                    res.innerHTML += "&#x1F449 " + c
+                }
+                res.innerHTML += '&#x1F3C1'    
+            }  
+
+        } 
+ 
     }
+    
+    
+    
 }
+
+
 
