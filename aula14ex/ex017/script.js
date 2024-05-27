214d8e09b90c127f85be0d339dab5046fce2c0c1
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-27 20:10:48 -0300

    terminei ex017

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,3 +1,19 @@
 function tabuada() {
+    var num = document.getElementById('num')
+    var tab = document.getElementById('tab')
+
+    var n = Number(num.value)
+
+    if (num.value.length == 0) {
+        window.alert('Por favor,digite um número')
+    } else{
+        tab.innerHTML = ''
+        for(var c = 1;c<=10;c++) {
+            var option = document.createElement('option')
+            tab.appendChild(option) 
+            option.text = `${n}x${c} = ${n*c}` //anotar
+        }
+    }
+    
     
 }
