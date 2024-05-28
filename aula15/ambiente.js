7d177556fc67007cd699cae8103143f0785cfa08
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-28 14:01:40 -0300

    terminei aula15

---
+++ aula15/ambiente.js
@@ -0,0 +1,8 @@
+let num = [1,8,3,4,9]
+
+num.push(7) //Adiciona o elemento 7 no vetor
+num.sort() //Coloca os números do vetor em ordem crescente
+
+console.log(`Aqui está o vetor: ${num}`)
+console.log(`O vetor tem ${num.length} elementos`)
+
---
+++ aula15/representacaovetor.js
@@ -0,0 +1,11 @@
+let valor = [1,5,9,6]
+valor.sort()
+/*
+    for (let pos = 0;pos<valor.length;pos++) {
+        console.log(valor[pos])
+    }
+*/
+
+for(let pos in valor) {
+    console.log(valor[pos])
+}
