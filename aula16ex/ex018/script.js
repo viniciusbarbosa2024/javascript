a70e1ddf6e2514a762c5595df42ef243cb40e0e3
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-05-30 06:03:45 -0300

    Quase acabando o ex018

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -1,7 +1,10 @@
+let num = document.getElementById('num')
+let acomp = document.getElementById('status')
+let n = []
+let res = document.getElementById('res')
+
 function feedback() {
-    let num = document.getElementById('num')
-    let status = document.getElementById('status')
-    let n = []
+
     
     if (num.value.length == 0 || num.value < 1 || num.value > 100 || n.indexOf(num.value) != -1) {
         window.alert('Valor inválido ou já adicionado na lista')
@@ -10,9 +13,63 @@ function feedback() {
         //Para o valor adicionado aparecer no select
         let option = document.createElement('option')
         option.text = `Valor ${num.value} adicionado`
-        status.appendChild(option)
+        acomp.appendChild(option)
 
+        //Guardando valor adicionado
         n.push(num.value)
+
+        //Limpando input de adicionar números
+        num.value = ''
     }
 
+}
+
+function analizador() {  
+    if(n.length == 0) {
+        window.alert('Adicione valores antes de finalizar')
+    } else {
+        for (let c=1; c <=5; c++) {
+            let criarp = document.createElement('p')
+            criarp.setAttribute('class','pres')
+            res.appendChild(criarp)
+        }
+
+        let p = document.getElementsByClassName('pres')
+
+        p[0].innerHTML = `Ao todo temos ${n.length} números adicionados`
+    
+        p[1].innerHTML = `O maior valor informado foi ${maiorvalor()}`
+
+        p[2].innerHTML = `O menor valor informado foi ${menorvalor()}`
+
+        p[3].innerHTML = `A soma de todos os valores é ${soma()}`
+
+        p[4].innerHTML = `A média dos valores é ${media().toFixed(2)}`
+    }
+    
+
+}
+
+function maiorvalor() {
+    n.sort()
+    return n[n.length -1]
+}
+
+function menorvalor() {
+    n.sort()
+    return n[0]
+}
+
+function soma() {
+    let s = 0
+    for(let pos in n) {
+        s += Number(n[pos])   
+    }
+    return s
+}
+
+function media() {
+    let m = 0
+    m = soma() / (n.length)
+    return m
 }
